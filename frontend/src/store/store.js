2a422d8e90bb09dc4b031242229e585d585dcd14
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-04-19 18:49:04 +0530

    add order reducer to Redux store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import cartReducer from "./slices/cartSlice";
 import searchReducer from "./slices/searchSlice";
+import orderReducer from "./slices/orderSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
     search: searchReducer,
+    order: orderReducer,
   },
 });
 
