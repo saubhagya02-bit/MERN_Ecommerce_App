497b40f6151b930e9640c136d939a8d73a3bbd6e
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-05-06 17:44:26 +0530

    restructure backend entry point to api/index.js (renamed from index.server.js)

---
+++ api/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import orderRoutes from "./routes/orderRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+//middlewares
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      process.env.FRONTEND_URL,
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(morgan("dev"));
+
+//API Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/order", orderRoutes);
+
+//Root
+app.get("/", (req, res) => {
+  res.send("<h1>>EliteMart API is running ✅</h1>");
+});
+
+//404 Handler
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: "Route not found" });
+});
+
+//Global Error Handler
+app.use((err, req, res, next) => {
+  console.error("Global Error:".red, err.message);
+  res.status(500).json({
+    success: false,
+    message: "Internal server error",
+    error: err.message,
+  });
+});
+
+//Start Server
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.DEV_MODE} mode on port ${PORT}`);
+});
+
+export default app;
